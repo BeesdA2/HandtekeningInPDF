61e3ac6885a4c03b910c6c5572cd156afdcf9836
mvmeel05 <m.van.meel@beesda2.nl>
2024-08-30 14:12:47 +0200

    handtekeningPlaatsen proefrit toegevoegd.

--- js/consumeWebserviceHandtekening.js
+++ js/consumeWebserviceHandtekening.js
@@ -150,8 +150,114 @@ const pdfBytes = await pdfDoc.save();
 }
 //handtekeningPlaatsen (origineelDocument,handTekening, xxText, yyText);
 
-async function handleHandtekeningPlaatsen (origineelDocument, handTekening, xxText, yyText)
+async function handtekeningPlaatsenProefrit (origineelDocument, handTekening, xxText, yyText) {
+  try{
+    
+ var pathName = '/www/profoundui/htdocs/signatures/'; 	   
+ var origineelDocumentFullname = pathName+origineelDocument;
+ var handTekeningFullname = pathName+handTekening;  
+ const pdfDoc = await PDFDocument.load(fs.readFileSync(origineelDocumentFullname));
+ 
+const img = await pdfDoc.embedPng(fs.readFileSync(handTekeningFullname));
+// Embed the Times Roman font
+const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+const origineelDocumentFilename = path.parse(origineelDocument).name;
+
+//console.log('handtekening plaatsen in PDF :origineelDocumentFilename: '+ origineelDocumentFilename); 
+
+//const imagePage = pdfDoc.insertPage(1);
+
+//var today= new Date().toLocaleDateString();
+//console.log(today); //Ouput: "23/06/2022"
+//var today= new Date().toLocaleTimeString();
+//console.log(today); //Ouput: "11:31:41"
+
+var today= new Date().toLocaleString('nl-NL', { timeZone: 'Europe/Amsterdam' });
+
+//console.log(today);   
+
+
+for (let i = 0; i < pdfDoc.getPageCount(); i++) {
+let imagePage='';
+imagePage = pdfDoc.getPage(i);
+//console.log(i+1)
+
+// console.log(imagePage.getWidth())
+
+let xx=imagePage.getWidth()
+
+//console.log(imagePage.getHeight())
+
+let yy=imagePage.getHeight()
+
+
+     
+   
+    var xxDrawImageAdd = 15; 
+    var xxDrawImageXXText = parseInt(xxText,10);
+      var xxDrawImage = xxDrawImageXXText -xxDrawImageAdd;
+     
+    // console.log('xxDrawImage: ' +xxDrawImage);
+     
+   var yyDrawImageAdd = 35;
+   var yyDrawImageYYText = parseInt(yyText,10);
+   var yyDrawImage = yyDrawImageYYText + yyDrawImageAdd;
+    
+   //console.log('yyDrawImage: '+yyDrawImage);
+    
+   
+    imagePage.drawImage(img, {
+      x: xx-xxDrawImage,
+      y: yy-yyDrawImage,
+      width: 70,
+       height: 40
+
+});
+
+    
+   var xxDrawTodayAdd = -10;
+   var xxDrawTodayXXText = parseInt(xxText,10);
+   var xxDrawToday = xxDrawTodayXXText + xxDrawTodayAdd;
+   
+  // console.log('xxDrawToday : '+ xxDrawToday);
+   
+  
+   var yyDrawTodayAdd = 48;
+   var yyDrawTodayYYText = parseInt(yyText,10);
+   var yyDrawToday = yyDrawTodayYYText + yyDrawTodayAdd;
+    
+   //console.log('yyDrawToday : '+ yyDrawToday);
+   
+   
+// Draw the string of today timestamp on the page
+     const fontSizeToday= 8;
+     imagePage.drawText(today , {
+       x: xx-xxDrawToday,
+       y: yy-yyDrawToday,
+       size: fontSizeToday,
+       font: timesRomanFont,
+       color: rgb(0, 0, 0),
+     })
+const pdfBytes = await pdfDoc.save();
+var pathToPDF = "";
+ const newFilePath = pathName + origineelDocumentFilename + '-handTekening.PDF'; 
+ fs.writeFileSync(newFilePath, pdfBytes);
+ 
+}
+   
+   } catch (e) {
+       console.error('handtekeningPlaatsen error:  '+e);
+   } finally {
+       console.log('Handtekening geplaatst in PDF!');
+   return ({ message: 'Handtekening geplaatst in PDF'});
+   }
+}
+
+async function handleHandtekeningPlaatsen (origineelDocument, handTekening, xxText, yyText, proefrit)
 {
+	if ((proefrit).trim() === 'N')
+	 {	
+	
     try{	
 	
 	
@@ -159,8 +265,21 @@ async function handleHandtekeningPlaatsen (origineelDocument, handTekening, xxTe
 	return (resolve);
     }
 	catch(err) {}
+    }
 	
+	if ((proefrit).trim() === 'J')
+	 {	
+	
+    try{	
+	
+	
+	var resolve = await handtekeningPlaatsenProefrit (origineelDocument,handTekening, xxText, yyText );
+	return (resolve);
+    }
+	catch(err) {}
+    }
 }
+
 
 
 module.exports = {
